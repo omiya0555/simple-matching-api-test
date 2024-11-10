151f0535bc39004c13e7a27bf7e96e43db0e19e4
omiya0555 <otoharadak@gmail.com>
2024-11-10 21:26:28 +0900

    Updated the migration file
    Because add columns to users table.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_image')->nullable();
+            $table->date('birth_date')->nullable(); 
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->string('location')->nullable(); 
+            $table->boolean('is_verified')->default(false); 
             $table->rememberToken();
             $table->timestamps();
         });
