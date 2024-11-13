aa8b80183af368196ef4286dfa65f92dbe3ceb26
omiya0555 <otoharadak@gmail.com>
2024-11-13 20:40:27 +0900

    Setup a closure to schedule that monitors the online status.
    It's was a mistake to create the App\Console\Kernel.php in Laravel 11.

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,22 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Redis;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    $onlineUsers = Redis::hgetall('online_users');
+    $currentTime = now()->timestamp;
+
+    // 現在時刻 - 最終アクティブ時刻 = 10分以上 なら削除
+    // 上の確認をオンラインユーザー数　繰り返す
+    foreach ($onlineUsers as $userId => $lastActive) {
+        if ($currentTime - (int)$lastActive > 600) {  // 10分以上経過　サービスへの定義検討
+            Redis::hdel('online_users', $userId);
+        }
+    }
+})->everyMinute();
